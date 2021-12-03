d7520e220f92942e243bad17aa1438f0071a0829
Igor <ilaptev@ya.ru>
2021-12-03 17:22:11 +0300

    feat: added ts in webpack and eslint

--- news-JS/src/components/controller/appLoader.ts
+++ news-JS/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '6d63371d574a489993fd1b4af190d130', // получите свой ключ https://newsapi.org/
         });
     }
 }
