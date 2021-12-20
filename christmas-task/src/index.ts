fb5339fe116aa346e5dfcb36154246858dad73ab
Igor <ilaptev@ya.ru>
2021-12-20 22:51:12 +0300

    feat: added alert window for cross-check inform

--- christmas-task/src/index.ts
+++ christmas-task/src/index.ts
@@ -3,3 +3,6 @@ import Header from './js/modules/header';
 
 StartPage.render();
 Header.control();
+
+// eslint-disable-next-line no-alert
+alert('Доброго времени суток! Уважаемый проверяющий, если есть такая возможность, отложи, пожалуйста проверку до среды/четверга. Очень хочу доделать поиск и сброс стилей. Заранее спасибо!');
