b5909bee598f36bebabacae3e48e3dd12156a798
Igor <ilaptev@ya.ru>
2021-12-24 21:35:18 +0300

    feat: added snowfall option

--- christmas-task/src/js/modules/treeOptions.ts
+++ christmas-task/src/js/modules/treeOptions.ts
@@ -5,7 +5,6 @@ class TreeOptions {
     treeSelectContainer.addEventListener('click', (e) => {
       const target = e.target as HTMLElement;
       if (target.classList.contains('tree')) {
-        console.log('target.dataset.tree = ', target.dataset.tree);
         mainTree.src = `../assets/tree/${target.dataset.tree}.png`;
       }
     });
@@ -18,7 +17,36 @@ class TreeOptions {
       const target = e.target as HTMLElement;
       if (target.classList.contains('bg')) {
         mainTreeContainer.style.backgroundImage = `url("../assets/bg/${target.dataset.bg}.jpg")`;
-        console.log('mainTreeContainer.style.backgroundImage = ', mainTreeContainer.style.backgroundImage);
+      }
+    });
+  }
+
+  static snowfall() {
+    const snowfallContainer = document.querySelector('.blur .page-container') as HTMLElement;
+    const snowFlake = document.createElement('i') as HTMLElement;
+    snowFlake.classList.add('fa');
+    snowFlake.classList.add('fa-snowflake-o');
+    snowFlake.style.left = `${Number(Math.random()) * Number(window.innerWidth)}px`;
+    snowFlake.style.animationDuration = `${Number(Math.random()) * 3 + 2}s`;
+    snowFlake.style.opacity = Number(Math.random()).toString();
+    snowFlake.style.fontSize = `${Number(Math.random()) * 10 + 10}px`;
+    snowfallContainer.appendChild(snowFlake);
+    setTimeout(() => {
+      snowFlake.remove();
+    }, 5000);
+  }
+
+  static startSnowfall() {
+    const snowButton = document.querySelector('.snow-control') as HTMLElement;
+    let interval: ReturnType<typeof setInterval>;
+    let snowfallStatus = false;
+
+    snowButton.addEventListener('click', () => {
+      snowfallStatus = !snowfallStatus;
+      if (snowfallStatus) {
+        interval = setInterval(this.snowfall, 50);
+      } else {
+        clearInterval(interval);
       }
     });
   }
@@ -26,6 +54,7 @@ class TreeOptions {
   static init() {
     this.setTree();
     this.backGround();
+    this.startSnowfall();
   }
 }
 
